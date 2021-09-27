d0fc7fb2bc5c7a2a2b1912e602df808d66f52afe
Marlon Yepes <protectfacebook.php@gmail.com>
2021-09-27 13:30:23 -0500

    importation script

--- src/db/scripts/creation.sh
+++ src/db/scripts/creation.sh
@@ -5,9 +5,6 @@ sudo /etc/init.d/postgresql restart
 # -- CONFIGURACIÓN DE VARIABLES GLOBALES
 APP_HOME="."
 APP_OUTPUT="import.log"
- 
-
- 
 PG_DBUSER_ADM="postgres"
 PG_DBUSER="postgres"
 PG_DBPASS="1234"
@@ -16,17 +13,18 @@ DBADMIN_SECRET_PASSWD
  
 # -- SE LIMPIA LA CONSOLA Y SE DESPLIEGA EL TITULO DEL PROGRAMA.
 clear
-echo "IMPORT"
-echo "======"
+echo "=========================================="
+echo "*********Database importation*************"
+echo "=========================================="
  
 # -- SE VERIFICA QUE LOS DOS PARAMETROS QUE INDICAN EL TIPO DE BASE 
 # DE DATOS A UTILIZAR (MYSQL O POSTGRESQL) Y EL ARCHIVO DUMP DE DONDE
 # IMPORTAR LA BASE DE DATOS HAYAN SIDO PASADOS POR EL USUARIO.
 if [ -z $1 ] || [ -z $2 ]; then
-    echo "Error en la ejecución! Faltan parámetros.."
-    echo "Ejemplo ejecución:"
+    echo "Execution error, missing parameters."
+    echo "Execution example:"
     echo
-    echo "  $ sh import.sh [mysql|pg] dump.sql"
+    echo "$ sh creation.sh <pg> <sql_file>" "<db_password>"
     echo
     exit
 fi
@@ -34,23 +32,23 @@ fi
 # -- SE COMPRUEBA QUE EL PRIMER PARÁMETRO RECIBIDO CORRESPONDA 
 # A LAS OPCIONES VÁLIDAS QUE SON mysql Y pg.
 if [ $1 != "pg" ]; then
-    echo "Error en la ejecución! las opciones para el primer parámetro 'pg' (postgres)."
-    echo "Ejemplo ejecución:"
+    echo "Execution error, first parameter = 'pg' (postgres)."
+    echo "Execution example"
     echo
-    echo "  $ sh import.sh [mysql|pg] dump.sql"
+    echo "  $ sh creation.sh pg <data_base>.sql <db_password>"
     echo
 exit
 fi
  
 # -- SE VERIFICA LA EXISTENCIA DEL ARCHIVO DUMP.
 if ! [ -f $APP_HOME/$2 ]; then
-    echo "Error en la ejecución! El archivo dump '$2' no existe."
+    echo "Execuition error, '$2'  does not exists."
     echo
     exit
 fi
 
 if [ -z $3 ]; then
-    echo "Db Password Not Found"
+    echo "Execution error, data base password not found."
     echo 
     echo
     exit
